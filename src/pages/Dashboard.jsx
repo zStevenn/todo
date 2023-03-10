00c136eb6dece74e78ae59d7d7b7ccb6f46e933c
zStevenn <stevenli.inbox@gmail.com>
2023-03-10 22:18:20 +0100

    Creates dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,9 @@
-import { MdAdd, MdList, MdKeyboardDoubleArrowRight } from 'react-icons/md';
+import {
+  MdAdd,
+  MdList,
+  MdKeyboardDoubleArrowRight,
+  MdLogout,
+} from 'react-icons/md';
 import { BiTask } from 'react-icons/bi';
 import { BsGear } from 'react-icons/bs';
 import Layout from '../components/Layout.jsx';
@@ -6,9 +11,12 @@ import Layout from '../components/Layout.jsx';
 export default function Dashboard() {
   return (
     <Layout>
-      <div className="grid gap-8 px-6 pt-6">
+      <div className="grid gap-8 px-6 pt-6 pb-12 text-gray-700">
         <div>
-          <h1 className="text-xl">Hee [User.name],</h1>
+          <div className="flex justify-between">
+            <h1 className="text-xl">Hee [User.name],</h1>
+            <MdLogout className="text-3xl" />
+          </div>
           <p className="text-base mt-2">
             In één oogopslag kan je je lijsten, taken en nog veel meer bekijken!
           </p>
@@ -35,6 +43,39 @@ export default function Dashboard() {
           <div className="grid gap-4">
             <div className="flex justify-between bg-melon rounded-md shadow-md px-3 py-2">
               <p className="text-base text-white">Boodschappen</p>
+              <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
+            </div>
+            <div className="flex justify-between bg-glaucous rounded-md shadow-md px-3 py-2">
+              <p className="text-base text-white">Sporten</p>
+              <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
+            </div>
+            <div className="flex justify-between bg-melon rounded-md shadow-md px-3 py-2">
+              <p className="text-base text-white">Eten</p>
+              <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
+            </div>
+            <div className="flex justify-between bg-glaucous rounded-md shadow-md px-3 py-2">
+              <p className="text-base text-white">Willekeurig item</p>
+              <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
+            </div>
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <h3 className="text-lg">Openstaande lijsten</h3>
+          <div className="grid gap-4">
+            <div className="flex justify-between bg-melon rounded-md shadow-md px-3 py-2">
+              <p className="text-base text-white">Boodschappen</p>
+              <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
+            </div>
+            <div className="flex justify-between bg-glaucous rounded-md shadow-md px-3 py-2">
+              <p className="text-base text-white">Sporten</p>
+              <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
+            </div>
+            <div className="flex justify-between bg-melon rounded-md shadow-md px-3 py-2">
+              <p className="text-base text-white">Eten</p>
+              <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
+            </div>
+            <div className="flex justify-between bg-glaucous rounded-md shadow-md px-3 py-2">
+              <p className="text-base text-white">Willekeurig item</p>
               <MdKeyboardDoubleArrowRight className="text-2xl text-white" />
             </div>
           </div>
