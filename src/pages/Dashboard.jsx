12153433cc2c40a570c8598d1fda7a931e80f7e4
zStevenn <stevenli.inbox@gmail.com>
2023-04-17 15:16:35 +0200

    Removes login form and adds button to redirect to login

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,80 +4,22 @@ import { BsGear } from 'react-icons/bs';
 import { auth } from '../firebase';
 import { Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import Input from '../components/Input';
-import { MdEmail, MdLock, MdLogin } from 'react-icons/md';
 import { useState } from 'react';
-import FormButton from '../components/FormButton';
 import StyledLink from '../components/StyledLink';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import Loading from '../components/Loading';
+import Button from '../components/Button';
 
 export default function Dashboard() {
   const [user, loading] = useAuthState(auth);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [formError, setFormError] = useState('');
-  const [signInWithEmailAndPassword, userLogin, loadingLogin, error] =
-    useSignInWithEmailAndPassword(auth);
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!email) {
-      setFormError('Email is required');
-      return;
-    }
-
-    if (!password) {
-      setFormError('Password is required');
-      return;
-    }
-
-    if (password && email) {
-      setFormError('');
-    }
-
-    try {
-      await signInWithEmailAndPassword(email, password);
-    } catch (error) {
-      setFormError(error.message);
-    }
-  };
 
   // Display if not an active user
   if (!user) {
     return (
       <>
-        {(loading || loadingLogin) && <Loading />}
-        <div className="h-screen w-screen flex flex-col mt-8 px-8 gap-8 items-center">
-          <h1>Je moet eerst inloggen om deze pagina te bekijken.</h1>
-          <form
-            className="grid place-items-center gap-6 w-full"
-            onSubmit={handleFormSubmit}
-          >
-            <h1 className="text-xl font-semibold">Log in</h1>
-            <Input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter Email..."
-              icon={<MdEmail />}
-            />
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter Password..."
-              icon={<MdLock />}
-            />
-            {formError && <p className="text-secondary">{formError}</p>}
-            <FormButton
-              variant="primary"
-              text="Inloggen"
-              type="submit"
-              icon={<MdLogin />}
-            />
-          </form>
+        {loading && <Loading />}
+        <div className="flex flex-col mt-8 px-8 gap-8 items-center text-center">
+          <h1>Je moet eerst inloggen om je eigen dashboard te bekijken.</h1>
+          <Button text="Naar inloggen" to="/login" />
           <StyledLink className="text-sm" to="/register">
             Geen account? Registreer hier
           </StyledLink>
